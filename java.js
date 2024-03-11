cca6731b5b5e85d9154f97a08747b166414435c8
Gissel Contreras <73136298+Gisselo@users.noreply.github.com>
2024-03-11 00:23:23 -0600

    Inicio

---
+++ java.js
@@ -0,0 +1,5 @@
+// Este es un comentario en JavaScript
+// Aquí puedes escribir tu código JavaScript
+
+// Ejemplo: Imprimir un mensaje en la consola
+console.log("Hola, este es mi primer archivo JavaScript");
